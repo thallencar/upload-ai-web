7d7567ae59e2aafcb39a3a73c974b8a518f5d01b
thallencar <alencar.thalita@gmail.com>
2023-09-14 16:42:44 -0300

    importando e estilizando o separator

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,30 @@
+import {Github} from 'lucide-react'
 import { Button } from "./components/ui/button";
+import { Separator } from '@radix-ui/react-separator';
 
 export function App() {
-  
 
+  const separatorStyles = {
+    backgroundColor: '#8A2BE2', 
+    width: '0.075em', 
+    height: '1.56em', 
+  };
+  
   return (
     <div className="px-6 pv-3 flex items-center justify-between border-b">
       <h1 className="text-xl font-bold">upload.ai</h1>
 
       <div className=" flex items-center gap-3">
-        <span className="text-sm font-thin text-muted-foreground">Desenvolvido com 💜 no NLW da Rocketseat</span>
-        <Button variant={"secondary"}>Github</Button>
+        <span className="text-sm font-thin text-muted-foreground">Desenvolvido com 💜 no NLW da Rocketseat
+        </span>
+
+        <Separator orientation='vertical' style={separatorStyles} className='h-6'/>
+
+
+        <Button variant={"outline"}>
+          <Github className='w-3.5 h-4 mr-2.5'/>
+          Github
+        </Button>
       </div>
     </div>
   )
