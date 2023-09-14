e3587719dc985a643997d287fd77ea2d0c2c6940
thallencar <alencar.thalita@gmail.com>
2023-09-14 16:11:43 -0300

    adicionando e estilizando o cabeçalho

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,13 @@ export function App() {
   
 
   return (
-    <Button>Teste</Button>
+    <div className="px-6 pv-3 flex items-center justify-between border-b">
+      <h1 className="text-xl font-bold">upload.ai</h1>
+
+      <div className=" flex items-center gap-3">
+        <span className="text-sm font-thin text-muted-foreground">Desenvolvido com 💜 no NLW da Rocketseat</span>
+        <Button variant={"secondary"}>Github</Button>
+      </div>
+    </div>
   )
 }
