0faede21cb95fc86c5ab3f49fae1660f39d38ab9
thallencar <alencar.thalita@gmail.com>
2023-09-15 16:36:05 -0300

    fixing bugs e criando conexão com bd

--- upload-ai-api/src/server.ts
+++ upload-ai-api/src/server.ts
@@ -1,13 +1,12 @@
-import { fastify } from 'fastify'
+import { fastify } from "fastify";
+
 
 const app = fastify()
 
-app.get('/', () => {
-    return 'Hello World'
-})
+
 
 app.listen({
-    port:3333,
+  port: 3333
 }).then(() => {
-    console.log('HTTP Server Running!')
+  console.log('HTTP Server Running!')
 })
